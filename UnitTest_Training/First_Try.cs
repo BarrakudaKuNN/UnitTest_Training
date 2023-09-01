00ec92d5effd9a23cedcd69ad7e89ef73a5a1967
risovanienasaite@gmail.com <risovanienasaite@gmail.com>
2023-09-01 18:37:33 +0300

    Useless project

---
+++ UnitTest_Training/First_Try.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest_Training
+{
+    public class First_Try
+    {
+        public static bool IsPrime(int number)
+        {
+            if (number < 2)
+            {
+                return false;
+            }
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ UnitTest_TrainingTests/First_TryTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTest_Training;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest_Training.Tests
+{
+    [TestClass()]
+    public class First_TryTests
+    {
+        [TestMethod()]
+        public void IsPrimeTest()
+        {
+            
+            Assert.IsTrue(First_Try.IsPrime(2));
+            Assert.IsTrue(First_Try.IsPrime(2));
+            Assert.IsTrue(First_Try.IsPrime(2));
+            Assert.IsTrue(First_Try.IsPrime(2));
+            Assert.IsTrue(First_Try.IsPrime(24));
+        }
+    }
+}
